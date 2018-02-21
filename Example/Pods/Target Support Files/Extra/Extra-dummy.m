ad347a4242a8cfe91913ed72745b1df92d38bacd
Jean-Charles SORIN <jean-charles@smartnsoft.com>
2018-02-21 20:21:07 +0100

    Remove pods folder

--- Example/Pods/Target Support Files/Extra/Extra-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Extra : NSObject
-@end
-@implementation PodsDummy_Extra
-@end
--- Example/Pods/Target Support Files/Pods-StarsKit_Example/Pods-StarsKit_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StarsKit_Example : NSObject
-@end
-@implementation PodsDummy_Pods_StarsKit_Example
-@end
